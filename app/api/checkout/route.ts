07944d040962fc8e174c28a51073f9c0c6235137
niklusrook <nikolas.vardeberg1@gmail.com>
2025-04-30 13:42:19 +0200

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import { urlFor } from "@/sanity/lib/image";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2025-03-31.basil",
 })
 
